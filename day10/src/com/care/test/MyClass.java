db8a9172f8a5b3f7741731bdbddf586499bae7e7
ChoiJiyeon <dkcl.8642@gmail.com>
2024-10-11 16:33:41 +0900

    package

---
+++ day10/src/com/care/test/MyClass.java
@@ -0,0 +1,16 @@
+package com.care.test; //보통 회사도메인 역순으로 패키지생성. com(care(test)) <- 이런 구성인것
+
+public class MyClass {
+	
+	public int n;
+	private int pri;
+	int def;
+	
+	
+	public void test() {
+		System.out.println("MyClass 기능");
+		
+	}
+		
+	
+}
---
+++ day10/src/java1/util/test1/MyClass.java
@@ -0,0 +1,8 @@
+package java1.util.test1;
+
+public class MyClass {
+	public void test() {
+		System.out.println("util 기능");
+		
+	}
+}
