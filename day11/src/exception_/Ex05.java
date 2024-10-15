d1e70086f4ab23d57a86a1989ff9b19392e05797
ChoiJiyeon <dkcl.8642@gmail.com>
2024-10-15 09:21:04 +0900

    예외

---
+++ day11/src/exception_/Ex05.java
@@ -0,0 +1,31 @@
+package exception_;
+
+import java.util.Scanner;
+
+public class Ex05 {
+public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	int x=10 , y=0;
+	try {
+		System.out.println( x / 2 );
+	} catch (Exception e) {
+		System.out.println("catch 실행");
+		return;
+	}finally {
+		System.out.println("stream 닫음!!!!");
+		input.close();
+	}
+	
+}
+}
+
+
+
+
+
+
+
+
+
+
+
